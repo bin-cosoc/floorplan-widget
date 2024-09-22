c3f2437c275e79f08fafe8db95fa091f7be398ff
nahum.santana <nahum.santana@newfold.com>
2024-09-22 17:20:40 -0400

    feat: all palma floors added

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -12,25 +12,15 @@
             </Suspense>
         </div>
         <div class="residence-info-container">
-            
             <div id="residenceInfo">
                 <h2 class="title">{{ store.selected.title }}</h2>
                 <h4 class="level">{{ store.selected.level }}</h4>
                 <p class="rooms">{{ store.selected.rooms }}</p>
-                <p>
-                    <b>Interior:</b>
-                    <span class="interior">{{ store.selected.interior }}</span>
-                </p>
-                <p>
-                    <b>Exterior:</b>
-                    <span class="exterior">{{ store.selected.exterior }}</span>
-                </p>
-                <p>
-                    <b>Total:</b>
-                    <span class="total">{{ store.selected.total }}</span>
-                </p>
+                <p><b>Interior:</b> {{ store.selected.interior }}</p>
+                <p><b>Exterior:</b> {{ store.selected.exterior }}</p>
+                <p><b>Total:</b> {{ store.selected.total }}</p>
             </div>
-            <a v-if="pdfUrl" id="downloadButton" :href="pdfUrl" download="FloorPlan.pdf">
+            <a v-if="pdfUrl" id="downloadButton" :href="pdfUrl" :download="pdfName">
                 DOWNLOAD FLOOR PLAN
             </a>
         </div>
@@ -51,6 +41,9 @@ export default {
     computed: {
         pdfUrl() {
             return this.store.selected.brochureUrl;
+        },
+        pdfName() {
+            return `FloorPlan_${this.store.selected.id}.pdf`;
         }
     }
 }
