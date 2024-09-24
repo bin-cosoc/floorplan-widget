107df3002809cbfbb502fb897ffeae671b1c95e8
Felix Sanchez <felixs.83@hotmail.com>
2024-09-24 12:59:19 -0400

    fix: add a new section of the floor plan details to below the floor plan diagram

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <Suspense>
       <Viewer></Viewer>
     </Suspense>
+    <Details class="mobile-only" />
   </div>
 </template>
 <script>
 import Aside from './components/Aside.vue';
 import Viewer from './components/Viewer.vue';
+import Details from './components/Details.vue';
 
 export default {
   components: {
+    Details,
     Aside,
     Viewer
-  },
+  }
 }
 </script>
